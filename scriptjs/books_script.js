d1a3b0772eae9836d91cfc1f8a60d33cd3dede01
OOleksandr Prylutskyi <fleks1986@gmail.com>
2022-03-17 11:33:57 +0200

    not find JSON file(((

--- scriptjs/books_script.js
+++ scriptjs/books_script.js
@@ -1,27 +1,12 @@
 const booksList = document.querySelector(['data-books-list']);
-const booksURL = "www.introligator.com.ua/books";
+const booksURL = 'http://www.introligator.com.ua/books';
 
-const booksRequest = new XMLHttpRequest();
-booksRequest.open('GET', booksURL);
-booksRequest.responseType = 'json';
-booksRequest.send();
 
-booksRequest.onload = function() {
-    let books = booksRequest.response;
-    showBooks(books);
+function load() {
+    let booksRequest = new XMLHttpRequest();
+    booksRequest.open('GET', booksURL);
+    booksRequest.responseType = 'json';
+    booksRequest.send();
 }
+console.log(load());
 
-function showBooks(booksCard) {
-    for(let i=0; i<booksCard.length; i++ ) {
-        let myImage = document.createElement('img');
-        let myName = document.createElement('h3');
-        let myAutor = document.createElement('h4');
-        let myAnotation = document.createElement('p');
-        let myType = document.createElement('p');
-        let myPage = document.createElement('p');
-        let myIsbn = document.createElement('p');
-        let myPrice = document.createElement('h4');
-
-        
-    }
-}
