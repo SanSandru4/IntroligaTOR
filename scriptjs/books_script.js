29f94ef6964d33243957713c0bff8cfaa36dfa99
OOleksandr Prylutskyi <fleks1986@gmail.com>
2022-03-17 13:53:35 +0200

    first out name books

--- scriptjs/books_script.js
+++ scriptjs/books_script.js
@@ -1,12 +1,17 @@
-const booksList = document.querySelector(['data-books-list']);
-const booksURL = 'http://www.introligator.com.ua/books';
+let booksList = document.querySelector('.main__books');
+console.log(booksList);
+let a;
 
+fetch('http://www.introligator.com.ua/books.json')
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+        // console.log(data[i].name);
+        let bookName = document.createElement('H2');
+        bookName.textContent = `${data[i].name}`;
+        console.log(bookName);
+        booksList.appendChild(bookName);
+    }
+  });
 
-function load() {
-    let booksRequest = new XMLHttpRequest();
-    booksRequest.open('GET', booksURL);
-    booksRequest.responseType = 'json';
-    booksRequest.send();
-}
-console.log(load());
 
