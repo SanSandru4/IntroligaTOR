3d1db365e98d5a3479c074ec1450f8ca81762f50
OOleksandr Prylutskyi <fleks1986@gmail.com>
2022-03-18 14:19:36 +0200

    cards is ready

--- scriptjs/books_script.js
+++ scriptjs/books_script.js
@@ -5,12 +5,33 @@ fetch('http://www.introligator.com.ua/books.json')
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
       let bookCard = document.createElement('div');
-      booksList.appendChild(bookCard);
+      appropriate(booksList, bookCard);
       bookCard.classList.add('books__card');
+      let bookImage = document.createElement('img');
+      bookImage.classList.add('books__img');
+      bookImage.setAttribute('src', `${data[i].image}`);
+      appropriate(bookCard, bookImage);
       let bookName = document.createElement('H2');
+      bookName.classList.add('books__name');
       bookName.textContent = `${data[i].name}`;
-      bookCard.appendChild(bookName);
+      appropriate(bookCard, bookName);
+      let bookAutor = document.createElement('p');
+      bookAutor.classList.add('books__autor');
+      bookAutor.textContent = `${data[i].autor}`;
+      appropriate(bookCard, bookAutor);
+      let bookPrice = document.createElement('p');
+      bookPrice.classList.add('books__price');
+      if (`${data[i].price}` == ' - ') {
+        bookPrice.textContent = 'Немає в наявності :(';
+      }
+      else {
+        bookPrice.textContent = `${data[i].price} грн.`;
+      }
+      appropriate(bookCard, bookPrice);
     }
   });
 
+  function appropriate(nod, val) {
+    nod.appendChild(val);
+  }
 
