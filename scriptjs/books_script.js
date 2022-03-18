5f510be4813a24bd8e53c504a08f43586fc84909
OOleksandr Prylutskyi <fleks1986@gmail.com>
2022-03-18 10:51:14 +0200

    card

--- scriptjs/books_script.js
+++ scriptjs/books_script.js
@@ -1,16 +1,15 @@
-let booksList = document.querySelector('.main__books');
-console.log(booksList);
-let a;
+let booksList = document.querySelector('[data-books-list]');
 
 fetch('http://www.introligator.com.ua/books.json')
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
-        // console.log(data[i].name);
-        let bookName = document.createElement('H2');
-        bookName.textContent = `${data[i].name}`;
-        console.log(bookName);
-        booksList.appendChild(bookName);
+      let bookCard = document.createElement('div');
+      booksList.appendChild(bookCard);
+      bookCard.classList.add('books__card');
+      let bookName = document.createElement('H2');
+      bookName.textContent = `${data[i].name}`;
+      bookCard.appendChild(bookName);
     }
   });
 
