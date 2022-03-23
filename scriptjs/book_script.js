c4788822074dc99a78f06e280f74de2b037c1a4f
OOleksandr Prylutskyi <fleks1986@gmail.com>
2022-03-23 15:24:02 +0200

    Start works books page

--- scriptjs/book_script.js
+++ scriptjs/book_script.js
@@ -1,5 +1,6 @@
 let booksList = document.querySelector('[data-books-list]');
-// console.log(bookLinkPage);
+let books_Name = document.querySelector('[data-book-name]');
+
 
 function appropriate(nod, val) {
   nod.appendChild(val);
@@ -8,35 +9,31 @@ function appropriate(nod, val) {
 fetch('http://www.introligator.com.ua/books.json')
   .then((response) => response.json())
   .then((data) => {
-    for (let i = 0; i < data.length; i++) {
       let bookCard = document.createElement('div');
       appropriate(booksList, bookCard);
       bookCard.classList.add('books__card');
-      let bookLinkPage = document.createElement('a');
-      appropriate(bookCard, bookLinkPage);
       let bookImage = document.createElement('img');
       bookImage.classList.add('books__img');
-      bookImage.setAttribute('src', `${data[i].image}`);
-      appropriate(bookLinkPage, bookImage);
+      bookImage.setAttribute('src', `${data[0].image}`);
+      appropriate(bookCard, bookImage);
       let bookName = document.createElement('H2');
       bookName.classList.add('books__name');
-      bookName.textContent = `${data[i].name}`;
+      bookName.textContent = `${data[0].name}`;
       appropriate(bookCard, bookName);
       let bookAutor = document.createElement('p');
       bookAutor.classList.add('books__autor');
-      bookAutor.textContent = `${data[i].autor}`;
+      bookAutor.textContent = `${data[0].autor}`;
       appropriate(bookCard, bookAutor);
       let bookPrice = document.createElement('p');
       bookPrice.classList.add('books__price');
-      if (`${data[i].price}` == ' - ') {
+      if (`${data[0].price}` == ' - ') {
         bookPrice.textContent = 'Немає в наявності :(';
       }
       else {
-        bookPrice.textContent = `${data[i].price} грн.`;
+        bookPrice.textContent = `${data[0].price} грн.`;
       }
       appropriate(bookCard, bookPrice);
-      bookLinkPage.setAttribute('href', `${data[i].link}`)
-    }
+      books_Name.textContent = `${data[1].name}`;
   });
 
 
